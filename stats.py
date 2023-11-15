5590b0a1c668219fac3daba0fdfceb3c1ea2f6cc
johnnyw <144480382+egggggggggggggggggggggg@users.noreply.github.com>
2023-11-15 10:59:22 -0500

    egeggee

---
+++ stats.py
@@ -0,0 +1,11 @@
+class Stats:
+    def char(self, health, attack, dodge, defense, atkspd, luck, wisdom):
+        self.health = health
+        self.attack = attack
+        self.dodge = dodge
+        self.defense = defense
+        self.atkspd = atkspd
+        self.luck = luck 
+        self.wisdom = wisdom
+    def health():
+        c
