ea45bcb399e71402cbc61c7730bf4b83745b3c8f
johnnyw <jwu38438@gmail.com>
2023-11-15 17:30:09 -0500

    Update stats.py

---
+++ stats.py
@@ -0,0 +1,33 @@
+#dodge 
+import random 
+class Dodge():
+    dodge = 100
+    if dodge != 0: 
+        for i in range(dodge):
+            x = random.randint(0, 100)
+            if x == 39:
+                print("DODGED")
+    elif dodge == 0:
+        print("no dodge")
+
+
+
+
+#health
+class Health():
+    health =  100
+    damage = 10
+    damage_taken = True
+    if damage_taken == True:
+         x = health - damage 
+         print(x)
+
+class Defense():
+    defense = 100
+    health = 100
+    damage_taken = True
+    damage = 10
+    if damage_taken == True:
+        x = defense * 0.03 
+        y = damage * x
+        print(health - x)
