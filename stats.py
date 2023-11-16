d1529cfee12b8912893b703745c8f2c3f30a37f1
johnnyw <144480382+egggggggggggggggggggggg@users.noreply.github.com>
2023-11-16 13:51:28 -0500

    eg

--- stats.py
+++ stats.py
@@ -1,17 +1,28 @@
 #dodge 
+import time
 import random 
+from c1ass import Class 
+
+time.sleep()
+
+
+health = 0
+damage = 0
+dodge = 0
+defense = 0
+atkspd = 0
+luck = 0
+mana = 0
+
+
 class Dodge():
-    dodge = 100
     if dodge != 0: 
         for i in range(dodge):
             x = random.randint(0, 100)
-            if x == 39:
+            if x == random.randint(0,100):
                 print("DODGED")
     elif dodge == 0:
         print("no dodge")
-
-
-
 
 #health
 class Health():
@@ -22,22 +33,24 @@ class Health():
          x = health - damage 
          print(x)
 
-class Defense():
-    defense = 100
-    health = 100
-    damage_taken = True
-    damage = 10
-    if damage_taken == True:
-        x = defense * 0.03 
-        y = damage * x
-        print(health - x)
+class Take_Damage():
+    def dmg():
+        edmg = 100 
+        damage_taken = True
+        if damage_taken == True:
+            x = defense * 0.03 
+            y = edmg * x
+            print(health - x)
 
-class Wisdom():
-    mana = 100
-    spell_cast = True
-    spell_cost = 10
-    if spell_cast == True:
-        print("Spell Cast")
-        print("-" +spell_cost+ "Mana")
-        mana = mana - spell_cost
-        print("current mana: " + mana + "")
+class MANA():
+    def spellcasting(): 
+        spell_cast = True
+        spell_cost = 10
+        if spell_cast == True:
+            print("Spell Cast")
+            print("-" +spell_cost+ "Mana")
+            mana = mana - spell_cost
+            print("current mana: " + mana + "")
+
+
+
