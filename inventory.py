72ff59f223aafb86ba3cb478070b3adbdd422089
johnnyw <144480382+egggggggggggggggggggggg@users.noreply.github.com>
2023-11-16 13:59:29 -0500

    eg

---
+++ inventory.py
@@ -0,0 +1,8 @@
+#make sure to create a global variable for the things here
+#json file has individual slots, everytime you recieve an item you update that json file. use the json file for invetory, etc. 
+
+class Inventory():
+    rer = 1 
+
+class Acessories():
+    egege = 1
