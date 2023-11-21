966d20ebfaa12c84004ff9e451db3fa31fdabffb
johnnyw <144480382+egggggggggggggggggggggg@users.noreply.github.com>
2023-11-21 13:55:26 -0500

    eg

---
+++ enemy.py
@@ -0,0 +1,68 @@
+class Enemy:
+    def __init__(self, ehealth, edamage, edodge, edefense, eatkspd, emana, ectch, ecrit ):
+        self.ehealth = ehealth
+        self.edamage = edamage 
+        self.edodge = edodge
+        self.edefense = edefense
+        self.etakspd = eatkspd
+        self.emana = emana
+        self.ectch = ectch
+        self.ecrit = ecrit
+    def create 
+
+class temp():
+    def heal():
+        ehealth - 10
+        if ehealth <= temphealth:
+            print(ehealth + pot)
+
+            
+temp.heal()
+
+ehealth = 20
+edamage = 1
+edodge = 100
+edefense = 0
+eatkspd = 0
+emana = 0
+ectch= 100
+ecrit = 10
+temphealth = ehealth
+
+class turn():
+    def player_turn():
+        print("Options")
+    def enemy_turn():
+        egg = 1
+    def equal_rate():
+        while enemy_alive == False or player_alive == False:
+            if turnvara == "player":
+                turn.player_turn()
+                health_check()
+                turn.enemy_turn()
+                health_check()
+            elif turnvara == "enemy":
+                turn.enemy_turn()
+                health_check()
+                turn.player_turn()
+                health_check()
+
+class Enemy_stats():
+    class attack():
+        def crit_check():
+            if crit == True:
+                Enemy_stats.attack.crit()
+        def crit():
+            print(edamage * ecrit)
+    class atkspd():
+        def roa():
+            if eatkspd == atkspd:
+                turnvara = "player"
+            if eatkspd < atkspd:
+                turnvara = "enemy"
+            turn.equal_rate()
+class health_check():
+    if health == 0:
+        enemy_alive == False
+    if ehealth == 0:
+        enemy_alive == True 
