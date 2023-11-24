8004fa4f2527817f96144cf6485c373668f98dd0
johnnyw <jwu38438@gmail.com>
2023-11-23 21:29:39 -0500

    g

--- loot.py
+++ loot.py
@@ -2,12 +2,25 @@ import json
 import random
 drop = True
 var = 1
+x=1
+y=0
 def enemy_spawn():
+    global x
+    global y
+    #list func to find spawn amounts of each thing
+    list = []
     with open('location.json','r') as e: 
         data = json.load(e)
-    enemy_list = data['location_1'][0]['enemies']
-    print(enemy_list)
-
+    #find number of types of enemies
+    eamount = data['location_1'][0]['enemies'][0]['types']
+    #get spawn amounts of each type of enemy and append to a list
+    while x != eamount+1:
+        list.append(data['location_1'][0]['enemies'][y][f'generic_enemy{x}'][0]['spawn_amount'])
+        x+=1
+        y+=1
+    #use indexing to take the numbers out of a list and pass it through another function later on
+    print(list[0])
+    
 
 
 def lootcheck():
@@ -15,12 +28,12 @@ def lootcheck():
         with open('enemies.json','r') as f:
             data = json.load(f)
         drop_chancetemp = data['generic_enemy1'][0]['loot_table'][0]['generic_loot1']
-        print((data['generic_enemy1'][0]["loot_table"][0,3]))
         drop_chance = 100/drop_chancetemp
         if random.randint(1,drop_chance) == random.randint(1, drop_chance):
             with open('loot.json','r') as r:
                 dete = json.load(r)
             print(dete[f'generic_loot{var}'][0]['name'])
-lootcheck()
+enemy_spawn()
+
 
 
