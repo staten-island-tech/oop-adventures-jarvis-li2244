d9a59aa036ed0061d2f0fc95f0fb0b72e770d6a3
johnnyw <144480382+egggggggggggggggggggggg@users.noreply.github.com>
2023-11-28 13:59:09 -0500

    efffffffffffffffffffffff

--- item.py
+++ item.py
@@ -2,17 +2,28 @@ import json
 
 with open('inventory.json', 'r') as f:
     inventory = json.load(f)
+with open('loot.json', 'r') as egg:
+    loot =json.load(egg)
 var = 1,2,3,4,5,6,7,8,9
+
+
 
 def hotbar():
     trigger = input("")
     return trigger
-def item_used():
+def tempe():
     trigger = hotbar()
-    temp = inventory['inventory'][0][f'slot{trigger}']
+    return inventory['inventory'][0][f'slot{trigger}']
+
+def item_used():
+    temp = tempe() #name of item
     print(f'selected item : {temp}')
-def item_usagecheck():
-    print('')
-
-
+    use = input("")
+    if use == "y" :
+        item_usage()
+def item_usage():
+    temp = tempe()
+    print('item used')
+    blah = loot[temp][0]['']
+def laewkwg_update_stats():
 item_used()
