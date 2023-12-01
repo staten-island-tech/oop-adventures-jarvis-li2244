9948838f0cdf484e23429f87be7f52325aae2bd7
johnnyw <144480382+egggggggggggggggggggggg@users.noreply.github.com>
2023-12-01 14:00:25 -0500

    egegg

--- exp.py
+++ exp.py
@@ -1,4 +1,4 @@
-from colorama import Fore
+
 color_red = "\033[91m"
 color_purple = "\33[95m"
 color_blue1 = "\33[34m"
@@ -11,78 +11,44 @@ color_yellow = "\33[93m"
 color_grey = "\33[37m"
 color_default = "\033[0m"
 
-
-class ASCII():
-    def sprite1():
-        print(Fore.WHITE +'HEALTH 80 / 100')
-        print(Fore.RED +'|████████████████▒▒▒▒|')
-    def sprite2():
-        print(Fore.WHITE +'MANA 80 / 100')
-        print(Fore.BLUE +'|████████████████▒▒▒▒|')
-
-
- 
-def type():
-    x = input("type of display: ")
-    return x
-def color():
-    y = input("type of color")
-    egg = "\033[91m"
-    if y == "RED".lower():
-        egg = "\033[91m"
-    return egg
-#attempt at health system 
-def display(): 
-    x = type()
-    y = color()
-    max = 100
-    current = 80
-    print(f'{x}: {current}/{max}')
-    yes_health = int(current/5) * "█"
-    no_health = int((max - current)/5) * "▒"
-    print(f'{y} {yes_health}{no_health}  \n')
 x = 31
-
-
 def health_display():
     max = 100
-    current = 82
-    print(f'ENEMY HEALTH: {current}/{max}')
-    yes_health = int(current/5) * "█"
-    no_health = int((max - current)/5) * "▒"
+    current = 80
+    teegreg = max/100
+    max1 =int(max/teegreg)
+    current1 = int(current/teegreg)
+    print(f'{color_default}ENEMY HEALTH: {current}/{max}')
+    yes_health = int(current1/5) * "█"
+    no_health = int((max1 - current1)/5) * "▒"
     print(f'\033[1;{x};40m{yes_health}{no_health}\n')
+y = 34
+def mana_display():
+    max = 1901904290
+    current = 1301904290
+    teegreg = max/100
+    max1 =int(max/teegreg)
+    current1 = int(current/teegreg)
+    print(f'{color_default}ENEMY MANA: {current}/{max}')
+    yes_health = int(current1/5) * "█"
+    no_health = int((max1 - current1)/5) * "▒"
+    print(f'\033[1;{y};40m{yes_health}{no_health}\n')
 
+#settings for health and mana bars to simplify them 102102010 to 100
+
+
+
+class Sprites():
+    def sprite1():
+        print("""
+         GHOST
+          ___
+        \/   \/
+        |\O O/|  
+        |  0  |
+        \     |
+         \  _/
+          \|
+        """)
+Sprites.sprite1()
 health_display()
-
-#health converter, changes a large number to 100
-num = int(input("NUMBER: "))
-def num_list():
-    fact = []
-    res = [int(x) for x in str(num)]
-    return fact
-def rounding(): 
-    egg = num_list()
-    tem1 = len(egg) - 1
-    print(egg)
-    for i in range(tem1): 
-        LEG = 1 * 10
-    if (egg[1]) > 4:
-        num = num + LEG
-    new_value = int(num/100) * 100
-    return new_value
-def factor(): 
-        num = rounding()
-        round(num)
-        print("Factors of ",num," (only 100): ")
-        for i in range(1, num + 1): 
-            if num % i == 0 : 
-                if i == 100:
-                    e = i
-                    egg = int(num/e)
-                    return egg
-            else:
-                print("NO FACTORS")
-def egg():
-    elege = factor()
-    print(elege)
-egg()
