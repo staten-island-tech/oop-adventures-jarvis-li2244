770ec6933c4a089c61a6e52c499eec6c02c21dcb
johnnyw <jwu38438@gmail.com>
2023-11-30 21:56:59 -0500

    eg

--- exp.py
+++ exp.py
@@ -1,5 +1,56 @@
-import random, json, time
+from colorama import Fore
+color_red = "\033[91m"
+color_purple = "\33[95m"
+color_blue1 = "\33[34m"
+color_blue2 = "\33[36m"
+color_blue3 = "\33[96m"
+color_green1 = "\033[92m"
+color_green2 = "\033[32m"
+color_brown = "\33[33m"
+color_yellow = "\33[93m"
+color_grey = "\33[37m"
+color_default = "\033[0m"
 
-test = ["EGG", "B", "eqewoefwo"]
-random.shuffle(test)
-print(test)
+
+class ASCII():
+    def sprite1():
+        print(Fore.WHITE +'HEALTH 80 / 100')
+        print(Fore.RED +'|████████████████▒▒▒▒|')
+    def sprite2():
+        print(Fore.WHITE +'MANA 80 / 100')
+        print(Fore.BLUE +'|████████████████▒▒▒▒|')
+
+
+ 
+def type():
+    x = input("type of display: ")
+    return x
+def color():
+    y = input("type of color")
+    egg = "\033[91m"
+    if y == "RED".lower():
+        egg = "\033[91m"
+    return egg
+#attempt at health system 
+def display(): 
+    x = type()
+    y = color()
+    max = 100
+    current = 80
+    print(f'{x}: {current}/{max}')
+    yes_health = int(current/5) * "█"
+    no_health = int((max - current)/5) * "▒"
+    print(f'{y} {yes_health}{no_health}  \n')
+x = 31
+
+
+def health_display():
+    max = 100
+    current = 80
+    print(f'ENEMY HEALTH: {current}/{max}')
+    yes_health = int(current/5) * "█"
+    no_health = int((max - current)/5) * "▒"
+    print(f'\033[1;{x};40m{yes_health}{no_health}\n')
+
+health_display()
+
--- temp/enemy.py
+++ temp/enemy.py
@@ -1,33 +1,34 @@
-ehealth = 20
-edamage = 1
-edodge = 100
-edefense = 0
-eatkspd = 0
-emana = 0
-ectch= 100
-ecrit = 10
-temphealth = ehealth
-pot = 10
-take_dmg = True
-
-  
+import json
+with open('enemies.json','r') as f:
+    enemies = json.load(f)
 
 
-class Damage():
-    def reg():
-        egg = 1
-    def defense():
-        dmgtaken = dmg * defense
-        
-
-
-
-
-class temp():
-    def heal():
-        L = temp
-        if L <= ehealth:
-            print(L + pot)
-
-            
-temp.heal()
+class Enemy():
+    def __init__(self, health, damage, dodge, defense, atkspd, mana, critchance, critdmg):
+        self.health = health
+        self.damage = damage
+        self.dodge = dodge
+        self.defense = defense
+        self.atkspd = atkspd
+        self.mana = mana
+        self.critchance = critchance
+        self.critdmg = critdmg
+    def register():
+        health = enemies['generic_enemy1'][0]['health']
+        damage = enemies['generic_enemy1'][0]['damage']
+        dodge = enemies['generic_enemy1'][0]['dodge']
+        defense = enemies['generic_enemy1'][0]['defense']
+        atkspd = enemies['generic_enemy1'][0]['atkspd']
+        mana = enemies['generic_enemy1'][0]['mana']
+        critchance = enemies['generic_enemy1'][0]['critchance']
+        critdmg = enemies['generic_enemy1'][0]['critdmg']
+        w = (health, damage, dodge, defense, atkspd, mana, critchance, critdmg)
+        w.register()
+    def health_display(self):
+        max = self.health
+        current = 80
+        print(f'ENEMY HEALTH: {current}/{max}')
+        yes_health = int(current/5) * "█"
+        no_health = int((max - current)/5) * "▒"
+        print(f'\033[1;{x};40m{yes_health}{no_health}\n')
+Enemy.health_display(self)
