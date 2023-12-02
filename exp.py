bb1a6c429a6157a9cb810605e2800bf8d592ad9a
johnnyw <jwu38438@gmail.com>
2023-12-02 12:57:39 -0500

    got lazy

--- exp.py
+++ exp.py
@@ -1,4 +1,5 @@
 import time
+import random
 color_red = "\033[91m"
 color_purple = "\33[95m"
 color_blue1 = "\33[34m"
@@ -10,28 +11,70 @@ color_brown = "\33[33m"
 color_yellow = "\33[93m"
 color_grey = "\33[37m"
 color_default = "\033[0m"
+x = 91
+health_dis = 0
+def health_display(max, current):
 
-x = 31
-def health_display():
-    max = 100
-    current = 80
     teegreg = max/100
     max1 =int(max/teegreg)
     current1 = int(current/teegreg)
     print(f'{color_default}ENEMY HEALTH: {current}/{max}')
     yes_health = int(current1/4) * "█"
     no_health = int((max1 - current1)/4) * "▒"
-    print(f'{color_default}╔═════════════════════════╗' )
-    print(f'║\033[1;{x};40m{yes_health}{no_health}{color_default}║')
-    print(f'{color_default}╚═════════════════════════╝' )
+    yes_health1 = int(current1/4) * "▌"
+    no_health1 = int((max1 - current1)/4) * "║"
+    if health_dis == 0:
+        print(f'{color_default}╔═════════════════════════╗' )
+        print(f'║\033[1;{x};40m{yes_health}{no_health}{color_default}║')
+        print(f'{color_default}╚═════════════════════════╝' )
+    elif health_dis == 1:
+        print(f'{color_default}╔═════════════════════════╗' )
+        print(f'║\033[1;{x};40m{yes_health1}{no_health1}{color_default}║')
+        print(f'{color_default}╚═════════════════════════╝' )
 def battle_gui():
     print(r"""
-╔═════════╗ ╔═════════╗ ╔═════════╗ ╔═════════╗
-║         ║ ║         ║ ║         ║ ║         ║
-║   ATK   ║ ║   RUN   ║ ║  EQUIP  ║ ║  ITEMS  ║ 
-║         ║ ║         ║ ║         ║ ║         ║  
-╚═════════╝ ╚═════════╝ ╚═════════╝ ╚═════════╝
+╔═════════╗ ╔═════════╗ 
+║         ║ ║         ║ 
+║   ATK   ║ ║   RUN   ║ 
+║         ║ ║         ║   
+╚═════════╝ ╚═════════╝ 
+╔═════════╗ ╔═════════╗
+║         ║ ║         ║
+║  EQUIP  ║ ║  ITEMS  ║
+║         ║ ║         ║  
+╚═════════╝ ╚═════════╝
             """)
+def actions():
+    ere =int(input(""))
+    atk_gui() if ere == 1 else(run_gui() if ere == 2 else(equip_gui() if ere == 3 else(items_gui() if ere == 4 else actions())))
+def run_gui():
+    #dodge will factor into whether you lose stuff when you run away or not 
+    dodge = int(100)
+    roll = int(100/dodge)
+    if random.randint(1, roll) == random.randint(1,roll):
+        print(r""" 
+
+        ╔═════════════════════════╗
+        ║     ESCAPE SUCESSFUL    ║
+        ╚═════════════════════════╝
+            """)
+    else:
+        print(r""" 
+        ╔═════════════════════════╗
+        ║      ESCAPE FAILED      ║
+        ╚═════════════════════════╝
+            """)
+    
+def atk_gui():
+    var1 = 4564456
+    print(f"""
+╔═════════════════════════╗
+║    {var1} DMG Dealt     ║
+╚═════════════════════════╝
+        """)
+def equip_gui():
+    print(r"""
+        """)
 def items_gui():
     item_name = "EGG"
     print(f'''
@@ -55,29 +98,6 @@ def items_gui():
 9. {item_name}                             
 ══════════════════════════════════
             ''')
-def backpack_gui():
-    item_name = "balls"
-    print(f'''
-══════════════════════════════════
-1. {item_name}                             
-══════════════════════════════════
-2. {item_name}                             
-══════════════════════════════════
-3. {item_name}                             
-══════════════════════════════════
-4. {item_name}                             
-══════════════════════════════════
-5. {item_name}                             
-══════════════════════════════════
-6. {item_name}                             
-══════════════════════════════════
-7. {item_name}                             
-══════════════════════════════════
-8. {item_name}                             
-══════════════════════════════════
-9. {item_name}                             
-══════════════════════════════════
-        ''')
 y = 34
 def item_desc():
     print(r"""
@@ -89,19 +109,16 @@ def item_desc():
 
 
             """)
-def mana_display():
-    max = 1901904290
-    current = 1301904290
-    teegreg = max/100
-    max1 =int(max/teegreg)
-    current1 = int(current/teegreg)
-    print(f'{color_default}ENEMY MANA: {current}/{max}')
-    yes_health = int(current1/5) * "█"
-    no_health = int((max1 - current1)/5) * "▒"
-    print(f'\033[1;{y};40m{yes_health}{no_health}\n')
 
 #settings for health and mana bars to simplify them 102102010 to 100
+def settings_GUI():
+    print(r"""
 
+
+
+
+
+        """)
 def attack():
     print("ATTACK!!")
     a = input("")
@@ -152,6 +169,16 @@ class Sprites():
         print("Credit for ASCII ART: (if any)")
         if input("CLICK ENTER TO START : "):
             Sprites.adjust()
+    def boss_screen():
+        print(r"""
+______________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+                                                                                 ____   ___  ____ ____    ____    _  _____ _____ _     _____ _   _   _ 
+                                                                                | __ ) / _ \/ ___/ ___|  | __ )  / \|_   _|_   _| |   | ____| | | | | |
+                                                                                |  _ \| | | \___ \___ \  |  _ \ / _ \ | |   | | | |   |  _| | | | | | |
+                                                                                | |_) | |_| |___) |__) | | |_) / ___ \| |   | | | |___| |___|_| |_| |_|
+                                                                                |____/ \___/|____/____/  |____/_/   \_\_|   |_| |_____|_____(_) (_) (_)        
+______________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+            """)
     def fight_screen():
         print(r"""
               
@@ -246,6 +273,20 @@ ________________________________________________________________________________
 
 
           """)
+    def sprite5():
+        print(r"""
+
+            DA DEAD ANT
+
+     \   / 
+      \ /  |\_|\_|\    ︵︵︵︵
+     |x x|/| \| \| \  /        \
+      \ / \|__|__|_ / \        /
+      { }  |  |  |     ︶︶︶︶
+
+
+
+          """)
     def placeholder0():
         print(r"""
 
@@ -255,9 +296,14 @@ ________________________________________________________________________________
            """)
 
 Sprites.sprite4()
-health_display()
+health_display(100, 80)
 battle_gui()
-items_gui()
+actions()
+Sprites.sprite5()
+health_display(100, 0)
+Sprites.boss_screen()
+time.sleep(1)
+Sprites.fight_screen()
 #might not need but code for updating dicts(inventory f
 # or example)
 #add new space below like this:
