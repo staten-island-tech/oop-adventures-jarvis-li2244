a7858bf92c87058a2dbf14d6e7946019e7caba62
johnnyw <jwu38438@gmail.com>
2023-12-17 19:15:48 -0500

    erggg

--- recipes.py
+++ recipes.py
@@ -13,28 +13,43 @@ def names():
         item_name.append(name)
         cyclone += 1
     return item_name
+def page_scroll():
+    numpad = input()
+    loste = []
+    fur = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    if numpad not in fur:
+        page_scroll()
+    else:
+        max_range = int(numpad) * 9
+        lowest_range = max_range - 9
+        for i in range(9):
+            loste.append(lowest_range)
+            lowest_range += 1
+    print(loste)
+    return loste
 def create_list():
+    recipe_page = page_scroll()
     item_name = names()
     print(f'''
 ══════════════════════════════════
-1. {item_name[0]}                             
+1. {item_name[recipe_page[0]]}                             
 ══════════════════════════════════
-2. {item_name[1]}                             
+2. {item_name[recipe_page[1]]}                             
 ══════════════════════════════════
-3. {item_name[2]}                             
+3. {item_name[recipe_page[2]]}
 ══════════════════════════════════
-4. {item_name[3]}                             
+4. {item_name[recipe_page[3]]}                             
 ══════════════════════════════════
-5. {item_name[4]}                             
+5. {item_name[recipe_page[4]]}                             
 ══════════════════════════════════
-6. {item_name[5]}                             
+6. {item_name[recipe_page[5]]}                             
 ══════════════════════════════════
-7. {item_name[6]}                             
+7. {item_name[recipe_page[6]]}                             
 ══════════════════════════════════
-8. {item_name[7]}                             
+8. {item_name[recipe_page[7]]}                             
 ══════════════════════════════════
-9. {item_name[8]}                             
+9. {item_name[recipe_page[8]]}                             
 ══════════════════════════════════
+        1 2 3 4 5 6 7 8...
 ''')
-print(len(recipes[0]))
 create_list()
