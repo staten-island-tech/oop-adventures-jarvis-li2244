f81ad992a665c6cfdb405699f865614577dd354e
johnnyw <144480382+egggggggggggggggggggggg@users.noreply.github.com>
2024-01-04 14:00:13 -0500

    egegegege

--- testcode2.py
+++ testcode2.py
@@ -1,4 +1,4 @@
-import json 
+"""import json 
 with open('mapinstance.json') as dropp:
     locatien = json.load(dropp)
 with open('enemies.json') as infile:
@@ -7,3 +7,17 @@ def enemy_spawn():
     for i, (v, k) in enumerate(locatien['enemies'][0].items()):
         print(i, v, k)
 enemy_spawn()
+
+"""
+
+import json, random
+with open('mapinstance.json', mode='r') as infile:
+    mapperjapper = json.load(infile)
+def spawnenemy():
+    for index, stuff in enumerate(mapperjapper['enemies']):
+        spawn = stuff[f'{index}'][0]['spawn_rate']
+        if random.randint(1, spawn) == random.randint(1, spawn):
+            print('enemy_spawned')
+spawnenemy()
+
+    
