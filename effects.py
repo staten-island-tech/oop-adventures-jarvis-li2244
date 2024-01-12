3d8b8b603707710892dae6d1a412f1dcc37faba4
johnnyw <144480382+egggggggggggggggggggggg@users.noreply.github.com>
2024-01-12 13:06:19 -0500

    efefeeqwcewwcw

---
+++ effects.py
@@ -0,0 +1,22 @@
+import json
+from interactivegui import *
+from methods import Modified_Functions  as mod
+class Effect:
+    def knock_out():
+        #prevent the enemy from using their turn
+        pass
+    def flame():
+        pass
+        #make the enemy take damage multiple times
+    def toxic():
+        pass
+        #make the enemy take damage multiple times
+    def equalize():
+        #make both classes take damage
+        pass
+    def blood_oath():
+        pass
+        #set health to 0, damage gets buffed insanely high, low damage
+    def hysteria():
+        pass
+        #set health to 1, damage becomes insanely high
